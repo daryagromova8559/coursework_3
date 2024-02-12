3d05a1d8f871c79bbdfb09c8149978458db00e44
dasha gromova <darya.gromova8559@gmail.com>
2024-02-12 17:30:43 +0300

    def operation_executed

--- utils/utils_operation.py
+++ utils/utils_operation.py
@@ -8,3 +8,13 @@ def load_operations(filename):
     """
     with open(filename, "r", encoding="utf-8") as file:
         return json.load(file)
+
+
+def operation_executed(filename):
+    '''Поиск одобренных операций'''
+    operation_executed_ = []
+    operation_equail = load_operations(filename)
+    for i in operation_equail:
+        if i.get("state") == "EXECUTED":
+            operation_executed_.append(i)
+    return operation_executed_
