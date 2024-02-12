c97b062a8b125fb594eaea7fc92a0ded550a6aa0
dasha gromova <darya.gromova8559@gmail.com>
2024-02-12 19:39:52 +0300

    add test_operation_unittest.py

---
+++ tests/test_operation_unittest.py
@@ -0,0 +1,7 @@
+import unittest
+
+from tests.test_operations_list import operations
+
+from utils.utils_operation import operation_executed, last_five_operations, parse_operations_data
+
+operation = 'tests.operations.json'
