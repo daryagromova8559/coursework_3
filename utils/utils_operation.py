e6be1be236b2768bcf8c71e2d0f02eb85264d95d
dasha gromova <darya.gromova8559@gmail.com>
2024-02-12 17:36:13 +0300

    def last_five_opertions

--- utils/utils_operation.py
+++ utils/utils_operation.py
@@ -18,3 +18,10 @@ def operation_executed(filename):
         if i.get("state") == "EXECUTED":
             operation_executed_.append(i)
     return operation_executed_
+
+
+def last_five_operations(operation_executed_):
+    '''Вывод последних 5-ти операций'''
+    sorted_operations = sorted(operation_executed_, key=lambda x: x["date"], reverse=True)
+    last_five = sorted_operations[0:5]
+    return last_five
