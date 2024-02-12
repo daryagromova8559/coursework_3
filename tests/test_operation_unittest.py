be37c01ffa68f3a0cb6506636a5e673c608bdd25
dasha gromova <darya.gromova8559@gmail.com>
2024-02-12 19:52:45 +0300

    def test_operation_executed

--- tests/test_operation_unittest.py
+++ tests/test_operation_unittest.py
@@ -5,3 +5,12 @@ from tests.test_operations_list import operations
 from utils.utils_operation import operation_executed, last_five_operations, parse_operations_data
 
 operation = 'tests.operations.json'
+
+
+class TestOperations(unittest.TestCase):
+
+    def test_operation_executed(self):
+        assert operation_executed(operation) == [{'state': 'EXECUTED'}, {'state': 'EXECUTED'}, {'state': 'EXECUTED'}]
+        print(operation_executed(operation))
+
+
