2e5cffd4a8d6ba344f203896793a8aff3a09388c
dasha gromova <darya.gromova8559@gmail.com>
2024-02-12 19:37:52 +0300

    def test_parse_operations_data

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -19,3 +19,13 @@ def test_last_five_operations():
                      '2019-10-30T01:49:52.939296',
                      '2019-08-08T21:58:06.688541',
                      '2019-06-14T19:37:49.044089']
+
+
+def test_parse_operations_data():
+    oper = []
+    for elem in parse_operations_data(operations):
+        oper.append(elem)
+    print(oper[0])
+    assert oper[0] == ('29.12.2018 Перевод организации\n'
+                      'Счет 7797 75** **** 1529 -> Счет **1891\n'
+                      '66263.93 руб.')
