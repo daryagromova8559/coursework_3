c21008f559dd3440b396159760c41691ae66c38e
dasha gromova <darya.gromova8559@gmail.com>
2024-02-12 19:09:33 +0300

    add test_operation.py

---
+++ tests/test_operation.py
@@ -0,0 +1,5 @@
+from utils.utils_operation import last_five_operations, operation_executed, parse_operations_data
+from tests.test_operations_list import operations
+
+operation = 'tests.operations.json'
+
