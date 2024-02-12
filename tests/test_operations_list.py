49b52e4ceb569ed80a19d804bb34feab24a0945c
dasha gromova <darya.gromova8559@gmail.com>
2024-02-12 19:05:19 +0300

    add test_operations_list.py

---
+++ tests/test_operations_list.py
@@ -0,0 +1,162 @@
+operations = [{
+    "id": 547682597,
+    "state": "EXECUTED",
+    "date": "2018-12-29T21:45:18.495053",
+    "operationAmount": {
+        "amount": "66263.93",
+        "currency": {
+            "name": "руб.",
+            "code": "RUB"
+        }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 77977573135347241529",
+    "to": "Счет 33062909508148771891"
+},
+    {
+        "id": 811920303,
+        "state": "EXECUTED",
+        "date": "2019-06-14T19:37:49.044089",
+        "operationAmount": {
+            "amount": "63150.74",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод со счета на счет",
+        "from": "Счет 73222753239048295679",
+        "to": "Счет 78544755774551298747"
+    },
+    {
+        "id": 509645757,
+        "state": "EXECUTED",
+        "date": "2019-10-30T01:49:52.939296",
+        "operationAmount": {
+            "amount": "23036.03",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод с карты на счет",
+        "from": "Visa Gold 7756673469642839",
+        "to": "Счет 48943806953649539453"
+    },
+    {
+        "id": 801684332,
+        "state": "EXECUTED",
+        "date": "2019-11-05T12:04:13.781725",
+        "operationAmount": {
+            "amount": "21344.35",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Открытие вклада",
+        "to": "Счет 77613226829885488381"
+    }, {
+        "id": 122284694,
+        "state": "EXECUTED",
+        "date": "2019-08-08T21:58:06.688541",
+        "operationAmount": {
+            "amount": "98657.83",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 99668626339273709694",
+        "to": "Счет 27219929444683698245"
+    },
+    {
+        "id": 154927927,
+        "state": "CANCELED",
+        "date": "2019-11-19T09:22:25.899614",
+        "operationAmount": {
+            "amount": "30153.72",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Maestro 7810846596785568",
+        "to": "Счет 43241152692663622869"
+    },
+    {
+        "id": 743628025,
+        "state": "CANCELED",
+        "date": "2018-06-04T06:59:55.424356",
+        "operationAmount": {
+            "amount": "978.31",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 54883981902864782073",
+        "to": "Счет 61834060137088759145"
+    },
+    {
+        "id": 743278119,
+        "state": "CANCELED",
+        "date": "2018-10-15T08:05:34.061711",
+        "operationAmount": {
+            "amount": "51203.12",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод с карты на карту",
+        "from": "MasterCard 1435442169918409",
+        "to": "Maestro 7452400219469235"
+    },
+    {
+        "id": 871921546,
+        "state": "CANCELED",
+        "date": "2019-02-14T03:09:23.006652",
+        "operationAmount": {
+            "amount": "47022.09",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Visa Classic 6216537926639975",
+        "to": "Счет 67667879435628279708"
+    },
+    {
+        "id": 373912477,
+        "state": "CANCELED",
+        "date": "2018-03-09T02:11:01.339352",
+        "operationAmount": {
+            "amount": "33249.01",
+            "currency": {
+                "name": "руб.",
+                "code": "RUB"
+            }
+        },
+        "description": "Открытие счета",
+        "to": "Счет 60979028617970883410"
+    },
+    {
+        "id": 720751477,
+        "state": "CANCELED",
+        "date": "2018-11-08T08:21:45.902633",
+        "operationAmount": {
+            "amount": "16872.46",
+            "currency": {
+                "name": "USD",
+                "code": "USD"
+            }
+        },
+        "description": "Перевод организации",
+        "from": "Счет 75743795418434298755",
+        "to": "Счет 80785963509390811744"
+    }]
